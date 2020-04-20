2b4abefaeb2f2a09a536fe1a10de9bdb0e959591
黄彪 <191059014@qq.com>
2020-04-20 17:55:22 +0800

    创建一些小工具，例如日志，traceid等

--- src/main/java/com/hb/st/sswt/common/Logger.java
+++ src/main/java/com/hb/st/sswt/common/Logger.java
@@ -2,6 +2,7 @@ package com.hb.st.sswt.common;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.annotation.JSONField;
 import com.hb.st.sswt.util.TraceIdUtil;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Marker;
@@ -51,8 +52,8 @@ public final class Logger {
      */
     public void info(String format, Object... arguments) {
         Object message = this.getLogMsg(format, arguments);
-        JSONObject jsonObject = this.buildLogTemplate(message, Thread.currentThread().getStackTrace(), "INFO");
-        this.delegateLogger.info(jsonObject.toJSONString());
+        CommonLogEntity commonLogEntity = this.buildLogTemplate(message, Thread.currentThread().getStackTrace(), "INFO");
+        this.delegateLogger.info(getJsonContent(commonLogEntity));
     }
 
     /**
@@ -63,8 +64,18 @@ public final class Logger {
      */
     public void error(String format, Object... arguments) {
         Object message = this.getLogMsg(format, arguments);
-        JSONObject jsonObject = this.buildLogTemplate(message, Thread.currentThread().getStackTrace(), "ERROR");
-        this.delegateLogger.info(jsonObject.toJSONString());
+        CommonLogEntity commonLogEntity = this.buildLogTemplate(message, Thread.currentThread().getStackTrace(), "ERROR");
+        this.delegateLogger.info(getJsonContent(commonLogEntity));
+    }
+
+    /**
+     * 转换成json
+     *
+     * @param commonLogEntity 日志实体
+     * @return 标准json
+     */
+    private String getJsonContent(CommonLogEntity commonLogEntity) {
+        return JSON.toJSONString(commonLogEntity);
     }
 
     /**
@@ -72,15 +83,15 @@ public final class Logger {
      *
      * @return 标准日志
      */
-    private JSONObject buildLogTemplate(Object message, StackTraceElement[] stacks, String logLevel) {
-        JSONObject jsonObject = new JSONObject();
-        jsonObject.put("content", message);
-        jsonObject.put("log_level", logLevel);
-        jsonObject.put("trace_id", TraceIdUtil.getTraceId());
-        jsonObject.put("file_name", stacks[2].getClassName());
-        jsonObject.put("file_line", stacks[2].getLineNumber());
-        jsonObject.put("log_date", (new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")).format(new Date(System.currentTimeMillis())));
-        return jsonObject;
+    private CommonLogEntity buildLogTemplate(Object message, StackTraceElement[] stacks, String logLevel) {
+        CommonLogEntity commonLogEntity = new CommonLogEntity();
+        commonLogEntity.setContent(message);
+        commonLogEntity.setTrace_id(TraceIdUtil.getTraceId());
+        commonLogEntity.setLog_date((new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS")).format(new Date(System.currentTimeMillis())));
+        commonLogEntity.setLog_level(logLevel);
+        commonLogEntity.setFile_name(stacks[2].getClassName());
+        commonLogEntity.setFile_line(stacks[2].getLineNumber());
+        return commonLogEntity;
     }
 
     /**
@@ -92,6 +103,75 @@ public final class Logger {
      */
     private Object getLogMsg(String format, Object[] argArray) {
         return MessageFormatter.arrayFormat(format, argArray, null).getMessage();
+    }
+
+    /**
+     * 标准日志实体类
+     *
+     * @param <T>
+     */
+    private class CommonLogEntity<T> {
+
+        @JSONField(ordinal = 1)
+        private T content;
+        @JSONField(ordinal = 2)
+        private String trace_id;
+        @JSONField(ordinal = 3)
+        private String log_date;
+        @JSONField(ordinal = 4)
+        private String log_level;
+        @JSONField(ordinal = 5)
+        private String file_name;
+        @JSONField(ordinal = 6)
+        private Integer file_line;
+
+        public T getContent() {
+            return content;
+        }
+
+        public void setContent(T content) {
+            this.content = content;
+        }
+
+        public String getTrace_id() {
+            return trace_id;
+        }
+
+        public void setTrace_id(String trace_id) {
+            this.trace_id = trace_id;
+        }
+
+        public String getLog_date() {
+            return log_date;
+        }
+
+        public void setLog_date(String log_date) {
+            this.log_date = log_date;
+        }
+
+        public String getLog_level() {
+            return log_level;
+        }
+
+        public void setLog_level(String log_level) {
+            this.log_level = log_level;
+        }
+
+        public String getFile_name() {
+            return file_name;
+        }
+
+        public void setFile_name(String file_name) {
+            this.file_name = file_name;
+        }
+
+        public Integer getFile_line() {
+            return file_line;
+        }
+
+        public void setFile_line(Integer file_line) {
+            this.file_line = file_line;
+        }
     }
 
 }

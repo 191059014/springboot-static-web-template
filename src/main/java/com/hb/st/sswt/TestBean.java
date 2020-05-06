0874761f01ac8a5091162739b0ba47bdcf69e641
黄彪 <191059014@qq.com>
2020-05-06 17:37:49 +0800

    整合unic

---
+++ src/main/java/com/hb/st/sswt/TestBean.java
@@ -0,0 +1,14 @@
+package com.hb.st.sswt;
+
+/**
+ * 注释
+ *
+ * @author Mr.huang
+ * @since 2020/5/6 17:31
+ */
+public class TestBean {
+
+    public TestBean() {
+        System.out.println("bean init success");
+    }
+}
